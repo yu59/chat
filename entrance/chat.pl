31ad39f006a078f119ca566ee557ee153574bee1
yu tobaru <e145759@gmail.com>
2017-04-21 14:45:42 +0900

    DBI comment out

--- entrance/chat.pl
+++ entrance/chat.pl
@@ -1,23 +1,64 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use DBI;
+#use DATA::DUMPER;
+# $c = DBI->connect("dbi:Pg:dbname=$dbname", "", "");
 
-# Documentation browser under "/perldoc"
+# our $DB_NAME = "postgres";
+#my $c = DBI->connect("dbi:Pg:dbname=$DB_NAME) or die "$!\n Error: failed to connect to DB.\n";
+# my $sth = $c->prepare("SELECT now();");
+# $sth->execute();
+
+#my $c = DBI->connect('dbi:Pg:dbname=finance;host=db.example.com','user','xyzzy',{AutoCommit=>1,RaiseError=>1,PrintError=>0});
+#print "2+2=",$c->selectrow_array("SELECT 2+2"),"\n";
+
+## Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
 my @entries = ();
+my @names =();
 get '/' => sub {
-  my $c = shift;
+  our $c = shift;
+  my $d = shift;
+
+#  $dbh->do('INSERT INTO test_table(a) VALUES (1)');
+
+ 
+#  my $dbh = DBI->connect("dbi:Pg:c=$c", "postgres", "1109yt");
+#  my $sth = $dbh->prepare("SELECT * from pg_tables");      
+#  $sth->execute();
+##while (my $ary_ref = $sth->fetchrow_arrayref) {
+##  my ($row) = @$ary_ref;
+##  print $row , "\n";
+##}
+  
+ # $sth = $dbh->prepare('INSERT INTO test_table(a) VALUES (?)');
+ # $sth->execute();
+
+#$sth->finish;
+#$dbh->disconnect;
+
   $c->stash(entries => \@entries); #配列
+ # $d->stash(names => \@names);
   $c->render('index');
+ # $d->render('index');
 };
 
 post '/post' => sub {
   my $c = shift;
+  #my $d = shift;
   my $entry = $c->param('body');
-  push @entries, $entry; #配列に格納
-  #$c->stash(entry => $entry);
+  #my $name = $d->('name_field');
+  #if 
+  unshift @entries, $entry; #配列に格納
+  #unshift @names, $name;
+  ##$c->stash(entry => $entry);
   $c->redirect_to('/');
+  #$d->redirect_to('/');
 };
+
+
+#$dbh->disconnect;
 
 app->start;
 __DATA__
@@ -27,10 +68,20 @@ __DATA__
 % title 'Input';  
   <h1>Chat</h1> 
   %= form_for '/post' => method => 'POST' => begin
+    %= "name : "
+    %= text_field 'name_field' 
+    <br>
+    %= 'address  :'
+    %= text_field 'body'
+    <br>    
+    %= 'comment:'
     %= text_field 'body'
     %= submit_button '投稿する'
 % end
 % # 配列を用いている　ここをPostgreSQLに保存するように変更する
+% #for my $name (@{$names}) {
+%  #  <p><%= $name %></p>
+% # }
 % for my $entry (@{$entries}) {
     <p><%= $entry %></p>
 % }
@@ -41,9 +92,14 @@ __DATA__
 % layout 'default';
 % title 'Output';
   <h1>Chat</h1> 
-  %= form_for '/post' => method => 'POST' => begin
-  %= text_field 'body'
-  %= submit_button '投稿する'  
+    %= form_for '/post' => method => 'POST' => begin
+    %= "name : "
+    %= text_field 'name_field' 
+    %= 'address  :'
+    %= text_field 'body'
+    %= 'comment'
+    %= text_field 'body'
+    %= submit_button '投稿する'  
 % end
 <p><%= $entry %></p>
 
